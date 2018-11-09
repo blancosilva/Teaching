4d41f1710f9652edb5fe846f9f73a0f5f5133df2
blancosilva <eseprimo@gmail.com>
2018-11-09 06:54:37 -0500

    Added third exam

---
+++ FA18/MA122/auto/MA122.FA18.3a.el
@@ -0,0 +1,22 @@
+(TeX-add-style-hook
+ "MA122.FA18.3a"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "12pt")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art12"
+    "amsmath"
+    "amsthm"
+    "amsfonts"
+    "amssymb"
+    "amsxtra"
+    "pgf"
+    "tikz")
+   (TeX-add-symbols
+    "radiobutton")
+   (LaTeX-add-amsthm-newtheorems
+    "problem"))
+ :latex)
+
